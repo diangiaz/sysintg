10119a2053f5b94b396744168e76ed545cd20201
diangiaz <giandiaz35@gmail.com>
2017-07-10 21:26:28 +0800

    Added login.php

---
+++ login.php
@@ -0,0 +1,60 @@
+<?php
+$login = false; 
+if (isset($_POST['submit'])){
+
+
+$name =$_POST['username'];
+$pass = $_POST['password'];
+$login = true;
+}
+
+
+
+if($login == true)
+{
+	header("Location: http://".$_SERVER['HTTP_HOST'].  dirname($_SERVER['PHP_SELF'])."/all.php");
+}
+
+
+
+ /*
+if (isset($_SESSION['badlogin']))
+  $_SESSION['badlogin']++;
+else
+  $_SESSION['badlogin']=1;
+  */
+
+
+/*End of main Submit conditional*/
+
+?>
+
+
+<html>
+	<head>
+		<title>Log-In</title>
+		<link href='css/login.css' rel='stylesheet'>
+		<link href='https://fonts.googleapis.com/css?family=Bitter:400,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Raleway:300,200italic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
+	</head>
+	
+	<body>
+		<div class="block">
+			
+			<h1></h1>
+			
+			<div class="message">
+				<p>Please log-in.</p>
+				
+				<div class="form">
+					<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+						<p><input type="text" name="username" placeholder="Username" size=20 required></p>
+						<p><input type="password" name="password" placeholder="Password" size=20 required></p>
+						<p><input type="submit" name="submit" value="LOG IN" size=20></p>
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
