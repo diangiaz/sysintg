dc4f38668967213d3682e48efd143a862a8dde9b
Student <Student@G304BPC24.manila.dlsu.edu.ph>
2017-07-31 16:03:05 +0800

    updated content.php and db

--- content.php
+++ content.php
@@ -17,12 +17,45 @@ if(empty($_SESSION['sortBy']))
 		   } else $_SESSION['AscDesc'] = "ASC";
 		   $_SESSION['sortBy'] = $_POST['sort'];
 	   }
+	   
+if (isset($_POST['school'])){
+		$schools = [];
+		foreach($_POST['school'] as $choice){
+			array_push($schools, $choice);
+		}
+		
+		if(count($schools) == 1){
+		$first = $schools[0];
+		$schoolQuery = "WHERE university = '$first'";
+		} else {	
+			$schoolQuery = "WHERE ";
+			foreach($schools as $school){
+				$schoolQuery = $schoolQuery . "university = '$school' OR ";
+			}
+			$schoolQuery = rtrim($schoolQuery,"OR ");
+		}
+
+$_SESSION['checks'] = $schools;		
+}
 ?>
+
+
+
+
+
 <div class="section">
                 
+				<form action="content.php" method="POST">
                   <div class="row">
+					<input type="checkbox" name="school[]" value="Ateneo De Manila University" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("Ateneo De Manila University", $schools)) echo "checked"; ?>> ADMU <br>
+					<input type="checkbox" name="school[]" value="De La Salle University" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("De La Salle University", $schools)) echo "checked"; ?>> DLSU <br>
+					<input type="checkbox" name="school[]" value="Mapua Institute of Technology" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("Maputa Institute of Technology", $schools)) echo "checked"; ?>> MIT <br>
+					<input type="checkbox" name="school[]" value="Lyceum of the Philippines University" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("Lyceum of the Philippines", $schools)) echo "checked"; ?>> LPU<br>
+					<input type="checkbox" name="school[]" value="University of Santo Tomas" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("University of Santo Tomas", $schools)) echo "checked"; ?>> UST <br>
+					<input type="checkbox" name="school[]" value="University of the Philippines" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("University of the Philippines", $schools)) echo "checked"; ?>> UP <br>
+					<input type="checkbox" name="school[]" value="Systems Technology Institute" <?php $schools = []; foreach ($_SESSION['checks'] as $check) array_push($schools, $check); if (in_array("Systems Technology Institute", $schools)) echo "checked"; ?>> STI<br>
+					<input type="submit" value="Update Table">
                     <div class="col-md-12">
-					<form action="content.php" method="POST">
                       <table>
                         <thead>						
                           <tr>
@@ -43,14 +76,15 @@ if(empty($_SESSION['sortBy']))
 								if (empty($_POST['search']))
 								$search 	= "";
 								else						
-								$search 	= $_POST['search'];
-								//$school		= $_POST['school'];
+								$search 	= $_POST['search'];	
 								$sortBy 	=  $_SESSION['sortBy'];
 								$ascdesc	=  $_SESSION['AscDesc'];
+								if (empty($schoolQuery))
+									$schoolQuery = "";
 								$query = "SELECT name as '0', lastname as '1', birthday as '2', university as '3'
-										FROM students
+										FROM table1
+										$schoolQuery
 										ORDER BY $sortBy $ascdesc";
-								
 								$result =	mysqli_query($dbc,$query);
 								
 								while($row=mysqli_fetch_array($result,MYSQL_ASSOC)){
@@ -62,6 +96,10 @@ if(empty($_SESSION['sortBy']))
 										echo "</tr>";
 								}
 							?>
+							
+							
+							
+							
                         </tbody>
 						
                       </table>
