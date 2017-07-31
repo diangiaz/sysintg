f574915c9540222e7a163021e246995c4666b44f
diangiaz <giandiaz35@gmail.com>
2017-07-31 19:47:20 +0800

    Complete without age filter

--- content.php
+++ content.php
@@ -1,3 +1,16 @@
+<style>
+button {
+     background:none!important;
+     color:inherit;
+     border:none; 
+     padding:0!important;
+     font: inherit;
+     /*border is optional*/
+     border-bottom:1px solid #444; 
+     cursor: pointer;
+}
+</style>
+
 <?php
 session_start();
 require_once('mysqlconnect.php');
@@ -49,65 +62,62 @@ $_SESSION['STI'] = (in_array("Systems Technology Institute", $schools));
 
 <div class="section">
                 
-				<form action="content.php" method="POST">
-                  <div class="row">
-					<input type="checkbox" name="school[]" value="Ateneo De Manila University" <?php if ($_SESSION['ADMU']) echo "checked"; ?>> ADMU <br>
-					<input type="checkbox" name="school[]" value="De La Salle University" <?php if ($_SESSION['DLSU']) echo "checked"; ?>> DLSU <br>
-					<input type="checkbox" name="school[]" value="Mapua Institute of Technology" <?php if ($_SESSION['MIT']) echo "checked"; ?>> MIT <br>
-					<input type="checkbox" name="school[]" value="Lyceum of the Philippines University" <?php if ($_SESSION['LPU']) echo "checked"; ?>> LPU<br>
-					<input type="checkbox" name="school[]" value="University of Santo Tomas" <?php if ($_SESSION['UST']) echo "checked"; ?>> UST <br>
-					<input type="checkbox" name="school[]" value="University of the Philippines" <?php if ($_SESSION['UP']) echo "checked"; ?>> UP <br>
-					<input type="checkbox" name="school[]" value="Systems Technology Institute" <?php if ($_SESSION['STI']) echo "checked"; ?>> STI<br>
-					<input type="submit" value="Update Table">
-                    <div class="col-md-12">
-                      <table>
-                        <thead>						
-                          <tr>
-                            <th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='1' style = 'text-decoration:none; color:#008000;'>First Name</th>
-                            <th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='2' style = 'text-decoration:none; color:#008000;'>Last Name</th>
-                            <th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='3' style = 'text-decoration:none; color:#008000;'>Birthday</th>
-                            <th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='4' style = 'text-decoration:none; color:#008000;'>University</th>
-                          </tr>
-                        </thead>
-                        </table>
-					</form>
-                    </div>
-                    <div class="col-sm-12" style = "height: 65%;overflow-y:auto;overflow-x:auto;margin-bottom:20px">
-                      <table border="1">
-                        <tbody>
-						
-							<?php
-								if (empty($_POST['search']))
-								$search 	= "";
-								else						
-								$search 	= $_POST['search'];	
-								$sortBy 	=  $_SESSION['sortBy'];
-								$ascdesc	=  $_SESSION['AscDesc'];
-								if (empty($schoolQuery))
-									$schoolQuery = "";
-								$query = "SELECT name as '0', lastname as '1', birthday as '2', university as '3'
-										FROM table1
-										$schoolQuery
-										ORDER BY $sortBy $ascdesc";
-								$result =	mysqli_query($dbc,$query);
-								
-								while($row=mysqli_fetch_array($result,MYSQL_ASSOC)){
-										echo "<tr>                                                    ";
-										echo "<td>{$row['0']}</td>		                              ";
-										echo "<td>{$row['1']}</td>                                    ";
-										echo "<td>{$row['2']}</td>                                    ";
-										echo "<td>{$row['3']}</td>                                    ";
-										echo "</tr>";
-								}
-							?>
-							
-							
-							
-							
-                        </tbody>
-						
-                      </table>
-					  <br>
-                  </div>
-                </div>
-              </div>
+	<form action="content.php" method="POST">
+    <div class="row">
+	<input type="checkbox" name="school[]" value="Ateneo De Manila University" <?php if ($_SESSION['ADMU']) echo "checked"; ?>> ADMU <br>
+	<input type="checkbox" name="school[]" value="De La Salle University" <?php if ($_SESSION['DLSU']) echo "checked"; ?>> DLSU <br>
+	<input type="checkbox" name="school[]" value="Mapua Institute of Technology" <?php if ($_SESSION['MIT']) echo "checked"; ?>> MIT <br>
+	<input type="checkbox" name="school[]" value="Lyceum of the Philippines University" <?php if ($_SESSION['LPU']) echo "checked"; ?>> LPU<br>
+	<input type="checkbox" name="school[]" value="University of Santo Tomas" <?php if ($_SESSION['UST']) echo "checked"; ?>> UST <br>
+	<input type="checkbox" name="school[]" value="University of the Philippines" <?php if ($_SESSION['UP']) echo "checked"; ?>> UP <br>
+	<input type="checkbox" name="school[]" value="Systems Technology Institute" <?php if ($_SESSION['STI']) echo "checked"; ?>> STI<br>
+	<input type="submit" value="Update Table">
+		<div class="col-md-12">
+		<table border = "1">
+			<thead>						
+			<tr>
+				<th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='1'>First Name</th>
+				<th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='2'>Last Name</th>
+				<th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='3'>Birthday</th>
+				<th class="col-sm-2"><button class='link-button' name='sort' type='submit' align='center' value='4'>University</th>
+			</tr>
+			</thead>
+			
+	</form>
+		</div>
+            <tbody>
+		
+			<?php
+				if (empty($_POST['search']))
+				$search 	= "";
+				else						
+				$search 	= $_POST['search'];	
+				$sortBy 	=  $_SESSION['sortBy'];
+				$ascdesc	=  $_SESSION['AscDesc'];
+				if (empty($schoolQuery))
+					$schoolQuery = "";
+				$query = "SELECT name as '0', lastname as '1', birthday as '2', university as '3'
+						FROM table1
+						$schoolQuery
+						ORDER BY $sortBy $ascdesc";
+				$result =	mysqli_query($dbc,$query);
+				
+				while($row=mysqli_fetch_array($result,MYSQL_ASSOC)){
+						echo "<tr>                                                    ";
+						echo "<td>{$row['0']}</td>		                              ";
+						echo "<td>{$row['1']}</td>                                    ";
+						echo "<td>{$row['2']}</td>                                    ";
+						echo "<td>{$row['3']}</td>                                    ";
+						echo "</tr>";
+				}
+			?>
+			
+			
+			
+			
+            </tbody>
+		
+		</table>
+    </div>
+</div>
+			  
