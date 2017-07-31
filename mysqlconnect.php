5b7563a84f3a53283ebd55d13667ee7bdafe2ab3
diangiaz <giandiaz35@gmail.com>
2017-07-31 19:38:40 +0800

    Complete without age filter

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -1,6 +1,8 @@
 <?php
-$connect=mysqli_connect("localhost","root","1234","students");
-if (!$connect) {
+$dbc=mysqli_connect("localhost","root","1234","students");
+if (!$dbc) {
 	die("connection Failed".mysqli_connect_error());
 }
+else
+	//echo "Connection Successfull";
  ?>
